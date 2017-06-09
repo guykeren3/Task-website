569344108cf4460b3c44b6c7a43996449f4686fe
guykeren3 <guykeren3@gmail.com>
2017-06-09 17:29:10 +0300

    finished scroll to top

--- main.js
+++ main.js
@@ -96,6 +96,25 @@ console.info(header);
 const goToTopBtn = document.querySelector('#go-to-top-link');
 console.info(goToTopBtn);
 
+function initGoToTopBtn() {
+  $("#go-to-top-link").hide();
+}
+
+initGoToTopBtn();
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // console.info($("#go-to-top-link").position().top);
+    // if ($("#go-to-top-link").position().top / $("#go-to-top-link").parent().height() * 100 > 10 ) {
+    //   $("#go-to-top-link").show();
+    // }
+
+    const doc = document.documentElement;
+
+    console.info((window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0));
+  });
+});
+
 $("#go-to-top-link").click(function () {
   $('html,body').animate({
       scrollTop: $(".header").offset().top
