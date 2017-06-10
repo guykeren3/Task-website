d66e551047bbec65e7d7ee611ef49a9a0b9ee332
guykeren3 <guykeren3@gmail.com>
2017-06-11 00:41:41 +0300

    done

--- main.js
+++ main.js
@@ -1,63 +1,6 @@
 /**
- * Created by Guy on 27/02/2017.
- */
-
-/**
  View
- */
-
-
-/*=====================================================================
-
- Templates
- //===================================================================*/
-
-// const membersTemplateWrapper = `
-// <span class="members-header">Taskboard Members</span>
-// <ul class="list-group col-sm-8 members-list">`;
-
-//   const membersTemplate =
-//     `<form class="members-page-form normal-mode">
-//         <input type="text" class="form-control edit-input" placeholder="Current">
-//       </form>
-//
-//       <div class="align-members-buttons">
-//
-//         <button type="button"
-//                 class="btn btn-info btn-sm none-edit">Edit
-//         </button>
-//
-//         <button type="button" class="btn btn-danger btn-sm none-edit delete-member">Delete</button>
-//
-//         <button type="button" class="btn btn-default btn-sm edit-buttons cancel-data">Cancel</button>
-//
-//         <button type="button" class="btn btn-success btn-sm edit-buttons save-data">Save</button>
-//
-//       </div>
-//
-//     </li>`;
-//
-//   const membersTemplateAddMember =
-//     `<li class="list-group-item">
-//
-//       <form class="members-page-form input-members">
-//         <input type="text" class="form-control input-fix-members" placeholder="Add new member">
-//         <button type="submit" class="btn btn-primary">Add</button>
-//       </form>
-//
-//     </li>
-//   </ul>
-//
-// </div>`;
-//
-//   const addListTemplate =
-//     `<div class="add-list-btn-wrapper">
-//     <button class="btn add-list-btn" id="add-list"> Add a list...</button>
-//   </div>`;
-//   /*=====================================================================
-//
-//    Feature related functions
-//    //===================================================================*/
+ **/
 
 //    chat box
 
@@ -67,22 +10,20 @@ function chatBoxClickHandler(event) {
 
   const chatBoxParent = document.querySelector('.chat-window-options');
 
-  // ulInsideButton.style.display = 'none';
-
   chatBoxParent.addEventListener('click', (e) => {
     const target = e.target;
-  console.info(target);
-  console.info(target.textContent);
-  if (target.innerText === "Chat") {
-    chatBox.style.display = 'block';
-    chatBoxCountdown();
-  }
+    console.info(target);
+    console.info(target.textContent);
 
+    if (target.innerText === "Chat") {
+      chatBox.style.display = 'block';
+      chatBoxCountdown();
+    }
 
-  if (target.innerText === 'E-mail') {
-    window.open('mailto:demo@demo.demo');
-  }
-});
+    if (target.innerText === 'E-mail') {
+      window.open('mailto:demo@demo.demo');
+    }
+  });
 }
 
 function chatBoxCountdown() {
@@ -90,6 +31,7 @@ function chatBoxCountdown() {
   let countdown = setInterval(function () {
     counter--;
     document.querySelector(".countdown").textContent = counter;
+
     if (counter <= -1) {
       clearInterval(countdown);
       chatBox.style.display = 'none';
@@ -97,9 +39,11 @@ function chatBoxCountdown() {
   }, 1000);
 }
 
+
 chatBoxClickHandler();
 
-const goToTopBtn = $(".go-to-top-button")
+
+const goToTopBtn = $(".go-to-top-button");
 
 function initGoToTopBtn() {
   goToTopBtn.hide();
@@ -108,6 +52,7 @@ function initGoToTopBtn() {
 initGoToTopBtn();
 
 $(document).ready(function () {
+
   $(window).scroll(function () {
 
     if ((document.body.scrollTop / document.body.offsetHeight * 100) > 50) {
